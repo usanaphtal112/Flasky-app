a2a6f643639d38f8b46d1bcd11a6cbcae8cbe5e0
Naphtal USABYUWERA <usanaphtal112@gmail.com>
2024-12-05 20:22:14 +0530

    Added flask project setup for medium and large project

---
+++ app/main/views.py
@@ -0,0 +1,36 @@
+from datetime import datetime, timezone
+from flask import render_template, session, redirect, url_for, flash
+from . import main
+from .forms import NameForm
+from .. import db
+from ..models import User
+
+
+@main.route("/", methods=["GET", "POST"])
+def index():
+    form = NameForm()
+
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session["known"] = False
+            flash("It's pleasure to meet you here")
+
+        else:
+            session["known"] = True
+            flash("It's Nice to meet you again!")
+
+        session["name"] = form.name.data
+        form.name.data = ""
+
+        return redirect(url_for(".index"))
+    return render_template(
+        "index.html",
+        current_time=datetime.now(timezone.utc),
+        form=form,
+        name=session.get("name"),
+        known=session.get("known", False),
+    )
--- hello.py
+++ hello.py
@@ -5,13 +5,37 @@ from datetime import datetime, timezone
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_mail import Mail
+from flask_mail import Message
+from threading import Thread
+import os
 
 app = Flask(__name__)
 
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+basedir = os.path.abspath(os.path.dirname(__file__))
 app.config["SECRET_KEY"] = "Hard to guess string"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
+    basedir, "data.sqlite"
+)
+app.config["MAIL_SERVER"] = "smtp.googlemail.com"
+app.config["MAIL_PORT"] = 587
+app.config["MAIL_USE_TLS"] = True
+app.config["MAIL_USERNAME"] = os.environ.get("MAIL_USERNAME")
+app.config["MAIL_PASSWORD"] = os.environ.get("MAIL_PASSWORD")
+app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
+
+app.config["FLASKY_MAIL_SUBJECT_PREFIX"] = "[Flasky]"
+app.config["FLASKY_MAIL_SENDER"] = "Flasky Admin <flasky@mail.com>"
+app.config["FLASKY_ADMIN"] = os.environ.get("FLASKY_ADMIN")
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+mail = Mail(app)
 
 
 class NameForm(FlaskForm):
@@ -24,16 +48,32 @@ def index():
     form = NameForm()
 
     if form.validate_on_submit():
-        old_name = session.get("name")
-        if old_name != None and old_name != form.name.data:
-            flash("It looks like you have changed your Names")
+        user = User.query.filter_by(username=form.name.data).first()
+        if user is None:
+            user = User(username=form.name.data)
+            db.session.add(user)
+            db.session.commit()
+            session["known"] = False
+            flash("It's pleasure to meet you here")
+            if app.config["FLASKY_ADMIN"]:
+                send_email(
+                    app.config["FLASKY_ADMIN"], "New User", "mail/new_user", user=user
+                )
+
+        else:
+            session["known"] = True
+            flash("It's Nice to meet you again!")
+
         session["name"] = form.name.data
+        form.name.data = ""
+
         return redirect(url_for("index"))
     return render_template(
         "index.html",
         current_time=datetime.now(timezone.utc),
         form=form,
         name=session.get("name"),
+        known=session.get("known", False),
     )
 
 
@@ -50,3 +90,46 @@ def page_not_found(e):
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template("500.html"), 500
+
+
+class Role(db.Model):
+    __tablename__ = "roles"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship("User", backref="role", lazy="dynamic")
+
+    def __repr__(self):
+        return "<Role %r>" % self.name
+
+
+class User(db.Model):
+    __tablename__ = "users"
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey("roles.id"))
+
+    def __repr__(self):
+        return "<User %r>" % self.username
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
+
+
+def send_async_email(app, msg):
+    with app.app_context():
+        mail.send(msg)
+
+
+def send_email(to, subject, template, **kwargs):
+    msg = Message(
+        app.config["FLASKY_MAIL_SUBJECT_PREFIX"] + subject,
+        sender=app.config["FLASKY_MAIL_SENDER"],
+        recipients=[to],
+    )
+    msg.body = render_template(template + ".txt", **kwargs)
+    msg.html = render_template(template + ".html", **kwargs)
+    thr = Thread(target=send_async_email, args=[app, msg])
+    thr.start()
+    return thr
